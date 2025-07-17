6340ac7deb1b8864ebcab55b7c94a3430ff72ef8
speedy123 <stephen.jr@selyuk.com>
2025-07-17 16:40:24 +0300

    add async dialog

--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@
 #include <windows.h>
 #include <shobjidl.h>
 #include <comdef.h>
+#include <condition_variable>
+#include <thread>
 
-void QuickDialogFail(const wchar_t* msg) {
+std::atomic dialogThreadCount { 0 };
+std::condition_variable threadZeroCond {};
+std::mutex threadZeroMutex {};
+
+void QuickDialogFail(const wchar_t *msg) {
     MessageBoxW(
         nullptr,
         msg,
         L"fuck",
         MB_ICONERROR | MB_OK
-        );
+    );
 }
 
-void QuickDialog(const wchar_t* msg) {
+void QuickDialog(const wchar_t *msg) {
     MessageBoxW(
         nullptr,
         msg,
@@ -23,11 +29,27 @@ void QuickDialog(const wchar_t* msg) {
     );
 }
 
-void ResultFail(HRESULT hr, const wchar_t* msg) {
+void QuickDialogAsync(const wchar_t *msg) {
+    dialogThreadCount.fetch_add(1);
+    std::thread {
+        [msg] {
+            QuickDialog(msg);
+
+            // checking if the old value is 1
+            if (dialogThreadCount.fetch_sub(1) == 1) {
+                std::lock_guard lock(threadZeroMutex); // locks mutex until destructor is called
+
+                threadZeroCond.notify_one();
+            }
+        }
+    }.detach();
+};
+
+void ResultFail(HRESULT hr, const wchar_t *msg) {
     if (FAILED(hr)) {
-        _com_error err {hr};
+        _com_error err{hr};
         // tchar is a wchar_t because UNICODE is defined
-        auto* errMsg = (wchar_t*) err.ErrorMessage();
+        auto *errMsg = (wchar_t *) err.ErrorMessage();
         QuickDialogFail(std::format(L"{}, HRESULT: {}", msg, errMsg).c_str());
         exit(1);
     }
@@ -35,16 +57,18 @@ void ResultFail(HRESULT hr, const wchar_t* msg) {
 
 class DialogEventHandler : public IFileDialogEvents {
     ULONG ref = 1;
+    HRESULT showExitCode = 0;
+
 public:
     virtual ~DialogEventHandler() = default;
 
-    HRESULT QueryInterface(const IID& riid, void** ppvObject) override {
+    HRESULT QueryInterface(const IID &riid, void **ppvObject) override {
         if (!ppvObject) {
             return E_POINTER;
         }
 
         if (riid == _uuidof(IFileDialogEvents) || riid == _uuidof(IUnknown)) {
-            *ppvObject = (IFileDialogEvents*)this;
+            *ppvObject = (IFileDialogEvents *) this;
 
             AddRef();
             return S_OK;
@@ -61,31 +85,32 @@ public:
         return count;
     }
 
-    HRESULT OnFileOk(IFileDialog* pfd) override {
-        wchar_t* wMsg = nullptr;
+    HRESULT OnFileOk(IFileDialog *pfd) override {
+        wchar_t *wMsg = nullptr;
 
         const HRESULT hr = pfd->GetFileName(&wMsg);
         ResultFail(hr, L"could not extract file name from file dialog");
 
-
-        QuickDialog(wMsg);
+        QuickDialogAsync(wMsg);
 
         CoTaskMemFree(wMsg);
 
         return S_OK;
     }
 
-    HRESULT OnFolderChanging(IFileDialog* pfd, IShellItem* psiFolder) override {return S_OK;}
+    HRESULT OnFolderChanging(IFileDialog *pfd, IShellItem *psiFolder) override { return S_OK; }
 
-    HRESULT OnFolderChange(IFileDialog* pfd) override {return S_OK;}
+    HRESULT OnFolderChange(IFileDialog *pfd) override { return S_OK; }
 
-    HRESULT OnSelectionChange(IFileDialog* pfd) override {return S_OK;}
+    HRESULT OnSelectionChange(IFileDialog *pfd) override { return S_OK; }
 
-    HRESULT OnShareViolation(IFileDialog* pfd, IShellItem* psi, FDE_SHAREVIOLATION_RESPONSE* pResponse) override {return S_OK;}
+    HRESULT OnShareViolation(IFileDialog *pfd, IShellItem *psi, FDE_SHAREVIOLATION_RESPONSE *pResponse) override {
+        return S_OK;
+    }
 
-    HRESULT OnTypeChange(IFileDialog* pfd) override {return S_OK;}
+    HRESULT OnTypeChange(IFileDialog *pfd) override { return S_OK; }
 
-    HRESULT OnOverwrite(IFileDialog* pfd, IShellItem* psi, FDE_OVERWRITE_RESPONSE* pResponse) override {return S_OK;}
+    HRESULT OnOverwrite(IFileDialog *pfd, IShellItem *psi, FDE_OVERWRITE_RESPONSE *pResponse) override { return S_OK; }
 };
 
 
@@ -94,35 +119,40 @@ int WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     ResultFail(hr, L"could not init com server");
-    IFileDialog* dialog = nullptr;
+    IFileDialog *dialog = nullptr;
 
     hr = CoCreateInstance(
         CLSID_FileOpenDialog,
         nullptr,
         CLSCTX_INPROC_SERVER,
         IID_IFileDialog,
-        (void**) &dialog
+        (void **) &dialog
     );
 
     ResultFail(hr, L"could not create file dialog object");
 
     // event handling object
-    IFileDialogEvents* events = new DialogEventHandler{};
+    IFileDialogEvents *events = new DialogEventHandler{};
     DWORD cookie = 0;
 
     hr = dialog->Advise(events, &cookie);
-
     ResultFail(hr, L"advise");
 
     hr = dialog->Show(nullptr);
-
     ResultFail(hr, L"show");
 
-    hr = dialog->Unadvise(cookie);
+    std::unique_lock lk (threadZeroMutex);
 
+    threadZeroCond.wait(lk, [] {
+        return dialogThreadCount.load() == 0;
+    });
+
+    // cleanup
+    hr = dialog->Unadvise(cookie);
     ResultFail(hr, L"unadvise");
 
     dialog->Release();
+
 
     CoUninitialize();
 }
